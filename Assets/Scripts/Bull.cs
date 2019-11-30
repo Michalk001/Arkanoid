96e2b331053fbdccd0e6531a78f2a0ca5362a99f
michalk001 <kostorz.michal@gmail.com>
2019-11-30 21:57:48 +0100

    fix gun bull
    was problem with collision (bad tag name)

--- Assets/Scripts/Bull.cs
+++ Assets/Scripts/Bull.cs
@@ -13,7 +13,7 @@ public class Bull : MonoBehaviour
     public Transform Parent { get; set; }
 
     private readonly string BrickTag = "Brick";
-    private readonly string WallTag = "Brick";
+    private readonly string WallTag = "Wall";
     private readonly float TimeToHidden = 2.5f;
     void Start()
     {
